2f81761e21436b057ef6a36c427c1c0dcc40e072
Samu Kangas <samu.kangas@metropolia.fi>
2025-11-24 01:52:12 +0200

    refactor: Remove unused font files, global styles, and configuration for a cleaner codebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,9 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Responses starter app",
-  description: "Starter app for the OpenAI Responses API",
-  icons: {
-    icon: "/openai_logo.svg",
-  },
+  title: 'VA Chat Service API',
+  description: 'API Backend for VA Chat Service',
 };
 
 export default function RootLayout({
@@ -28,12 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="flex h-screen bg-gray-200 w-full flex-col  text-stone-900">
-          <main>{children}</main>
-        </div>
+      <body>
+        <main>{children}</main>
       </body>
     </html>
   );
